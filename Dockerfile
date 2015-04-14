00a593d20cd08afbfb05997bdefc2d058c56242d
nchronas <nchronas@gmail.com>
2015-04-14 14:17:56 +0300

    event edtection

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN apt-get update -y
 RUN apt-get update && apt-get install -y \
 python python-pip python-dev python-dbus python-flask \
 dropbear \
+nano \
 && apt-get clean && rm -rf /var/lib/apt/lists/*
 RUN pip install RPi.GPIO
 COPY . /app
--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 import time
 import RPi.GPIO as GPIO
 
-#def my_callback(channel):
-#    print('This is a edge event callback function!')
-#    print('Edge detected on channel %s'%channel)
-#    print('This is run in a different thread to your main program')
+def my_callback(channel):
+    print('This is a edge event callback function!')
+    print('Edge detected on channel %s'%channel)
+    print('This is run in a different thread to your main program')
 
 print "Hello from Resin"
 
-#GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
-#GPIO.setup(40, GPIO.IN)
+GPIO.setup(40, GPIO.IN)
 
-#GPIO.add_event_detect(40, GPIO.RISING, callback=my_callback)  # add rising edge detection on a channel
+GPIO.add_event_detect(40, GPIO.RISING, callback=my_callback)  # add rising edge detection on a channel
 
 while True:
 	time.sleep(1)
