cca3c90dbbd79dc9ccc63a0a9c1e459f4f5deb56
nchronas <nchronas@gmail.com>
2015-04-15 13:57:32 +0300

    added funcionality

--- server.py
+++ server.py
@@ -10,21 +10,22 @@ from urlparse import urlparse, parse_qs
 
 
 def my_callback(channel):
-#    print('This is a edge event callback function!')
-#    print('Edge detected on channel %s'%channel)
-#    print('This is run in a different thread to your main program')
+    timelog = time.time() - logStart
+    print "event", channel, GPIO.input(channel), timelog 
 
 print "Hello from Resin"
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(40, GPIO.IN)
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#GPIO.add_event_detect(40, GPIO.RISING, callback=my_callback)  # add rising edge detection on a channel
+
+GPIO.add_event_detect(40, GPIO.RISING, callback=my_callback)  # add rising edge detection on a channel
 
 
 PORT = 80
 
+sampleSize = 1024
 log = False
     
 class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -36,8 +37,9 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         print 'server request'
 
         if self.path[0:5]=='/start':
-			global log
-            print "started logging" 
+			global log, logStart
+            print "started logging"
+            logStart = time.time() 
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
@@ -62,15 +64,31 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             return 
 
         else if self.path[0:4]=='/log':
-            print "hello my friend" 
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
             self.wfile.write(''' ''')
-            return        
+            return 
+               
+        else if self.path[0:7]=='/sample':
+            global sampleSize
+            test=parse_qs(urlparse(self.path).query)
+            print test, "get values"
+            if test  :
+                sampleSize=test['size']
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(''' ''')
+            return 
             
         else:
         #serve files, and directory listings by following self.path from
         #current working directory
             SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
+
+httpd = SocketServer.ThreadingTCPServer(('', PORT),CustomHandler)
+ 
+print "serving at port", PORT
+httpd.serve_forever()
